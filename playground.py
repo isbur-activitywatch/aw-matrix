59b61a4d1e9e2c440362fd866969f67c733e6d3a
Igor Burenkov <isburenkov@gmail.com>
2020-06-29 23:30:46 +0300

    PYCALLGRAPH_1

--- playground.py
+++ playground.py
@@ -1,16 +1,7 @@
-# from .tweaks.main import Function_With_Prepositions
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
 
-# proto = "Fetch | last-events /originating_at/ bucket"
-# def foo(obj = "bzz", originating_at = "baz"):
-#     print(obj)
-#     print(originating_at)
-
-# Fetch = Function_With_Prepositions(foo, proto)
-# last_events = "foo"
-# bucket = "bar"
-# Fetch | last_events /originating_at/ bucket
-
-
-# reload(tweaks.Phrases)
-# from .Phrases import Function_With_Prepositions
-# Fetch = Function_With_Prepositions(foo, proto)  
+with PyCallGraph(output=GraphvizOutput()):
+    import tick
+    import tweaks
+    tick.tick()
